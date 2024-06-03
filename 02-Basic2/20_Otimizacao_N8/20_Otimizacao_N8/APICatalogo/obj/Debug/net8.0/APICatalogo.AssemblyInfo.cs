36517dd6a9049f58240b67f90f9172e32f91b0f1
Matheus Emanuel <matheus.emanuelsc@gmail.com>
2024-06-03 20:58:17 -0300

    Categorias Repositorio especifico

--- 02-Basic2/20_Otimizacao_N8/20_Otimizacao_N8/APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ 02-Basic2/20_Otimizacao_N8/20_Otimizacao_N8/APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+652ed069583cced40f1add452ffe2bafecfa22a5")]
-[assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
+[assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
+[assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
---
+++ 02-Basic2/APICatalogo/Repository/CategoriaRepository.cs
@@ -0,0 +1,62 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+
+namespace APICatalogo.Repository
+{
+    public class CategoriaRepository : ICategoriaRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriaRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        IEnumerable<Categoria> ICategoriaRepository.GetCategorias()
+        {
+           return _context.Categorias.ToList();
+
+
+        }
+        Categoria ICategoriaRepository.GetCategoria(int id)
+        {
+            return _context.Categorias.FirstOrDefault(x => x.CategoriaId == id);
+        }
+
+
+        Categoria ICategoriaRepository.Create(Categoria categoria)
+        {
+            if (categoria is null)
+            {
+                throw new ArgumentNullException(nameof(categoria));
+            }
+            _context.Categorias.Add(categoria);
+            _context.SaveChanges();
+            return categoria;
+        }
+        Categoria ICategoriaRepository.Update(Categoria categoria)
+        {
+            if (categoria is null)
+            {
+                throw new ArgumentNullException(nameof(categoria));
+            }
+            _context.Entry(categoria).State= Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+            return categoria;
+        }
+
+        Categoria ICategoriaRepository.Delete(int id)
+        {
+            var categoria = _context.Categorias.Find(id);
+            if (categoria is null)
+            {
+                throw new ArgumentNullException(nameof(categoria));
+            }
+            _context.Remove(categoria);
+            _context.SaveChanges();
+            return categoria;
+        }
+       
+    }
+}
