c64fa6f7b59698ef6549d9de6d10c3d09231820a
matheus emanuelsc <matheus.emanuelsc@gmail.com>
2024-07-22 20:31:03 -0300

    Complemento

--- 04-Pagination/6_Paginacao_Incrementa/5_Pagination_Inc/APICatalogo/Pagination/PaginationParameters.cs
+++ 04-Pagination/6_Paginacao_Incrementa/5_Pagination_Inc/APICatalogo/Pagination/PaginationParameters.cs
@@ -5,8 +5,21 @@ using System.Threading.Tasks;
 
 namespace APICatalogo.Pagination
 {
-    public class PaginationParameters
+    public abstract class  PaginationParameters
     {
-        
+        const int maxPageSize = 50;
+        public int PageNumber { get; set; } = 1;
+        private int _pageSize = maxPageSize;
+        public int PageSize
+        {
+            get
+            {
+                return _pageSize;
+            }
+            set
+            {
+                _pageSize = (value > maxPageSize) ? maxPageSize : value;
+            }
+        }
     }
 }
