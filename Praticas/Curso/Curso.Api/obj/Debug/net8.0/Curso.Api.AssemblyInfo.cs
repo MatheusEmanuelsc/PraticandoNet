ffcb253a43c15d0f9a77019daac9967277216ff4
Matheus Emanuel <matheus.emanuelsc@gmail.com>
2024-08-15 13:21:08 -0300

    ajuste

--- Praticas/Curso/Curso.Api/obj/Debug/net8.0/Curso.Api.AssemblyInfo.cs
+++ Praticas/Curso/Curso.Api/obj/Debug/net8.0/Curso.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Curso.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+734ddbd10693598d3ff33c71db3bde340470e905")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e3c8822731ee2b910bb50356c087d2266226d7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Curso.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Curso.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
