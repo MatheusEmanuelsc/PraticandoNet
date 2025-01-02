259f35db3a650069d1043ac894c5408d9dd54d25
matheus emanuelsc <matheus.emanuelsc@gmail.com>
2025-01-02 16:56:34 -0300

    culture language

---
+++ Praticas/ProjetoCash/CashFlow/src/CashFlow.Api/Middleware/CultureMiddleware.cs
@@ -0,0 +1,31 @@
+using System.Globalization;
+
+namespace CashFlow.Api.Middleware;
+
+public class CultureMiddleware
+{
+    private readonly RequestDelegate _next;
+    public CultureMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        var supportedLanguages = CultureInfo.GetCultures(CultureTypes.AllCultures).ToList();
+        var requestCulture = httpContext.Request.Headers.AcceptLanguage.FirstOrDefault();
+        
+        
+        var cultureInfo = new CultureInfo("en");
+        if (string.IsNullOrWhiteSpace(requestCulture) == false
+            &&  supportedLanguages.Exists(language => language.Name.Equals(requestCulture))
+            )
+        {
+            cultureInfo = new CultureInfo(requestCulture);
+        }
+
+        CultureInfo.CurrentCulture = cultureInfo;
+        CultureInfo.CurrentUICulture = cultureInfo;
+        await _next(httpContext);
+    }
+}
--- Praticas/ProjetoCash/CashFlow/src/CashFlow.Domain/obj/Debug/net8.0/CashFlow.Domain.AssemblyInfo.cs
+++ Praticas/ProjetoCash/CashFlow/src/CashFlow.Domain/obj/Debug/net8.0/CashFlow.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5bd52e1f7a91d57004e21f83d7bbdc3d0aa9ff6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71de43b30c34cdce993bfd7e1d2a7717b1cd6057")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
