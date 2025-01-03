5c4a0a98c4c56dd5273d3690ced3dfa99fbc5a4a
matheus emanuelsc <matheus.emanuelsc@gmail.com>
2025-01-03 18:21:49 -0300

    test

--- Praticas/ProjetoCash/CashFlow/src/CashFlow.Domain/obj/Debug/net8.0/CashFlow.Domain.AssemblyInfo.cs
+++ Praticas/ProjetoCash/CashFlow/src/CashFlow.Domain/obj/Debug/net8.0/CashFlow.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71de43b30c34cdce993bfd7e1d2a7717b1cd6057")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+259f35db3a650069d1043ac894c5408d9dd54d25")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
