42610351bbc1e0c19167a4562ac862fc5c180469
matheus emanuelsc <matheus.emanuelsc@gmail.com>
2025-04-05 01:46:19 -0300

    migração

---
+++ Praticas/ListaTarefas/src/ListaTarefas.Api/Controllers/TarefasController.cs
@@ -0,0 +1,75 @@
+using ListaTarefas.Api.Entities;
+using ListaTarefas.Api.Repository;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace ListaTarefas.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarefasController : ControllerBase
+    {
+        private readonly IRepository _repository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public TarefasController( IRepository repository, IUnitOfWork unitOfWork )
+        {
+            _repository = repository;
+            _unitOfWork = unitOfWork;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+           var result =await _repository.GetALlTarefasAsync();
+           return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _repository.GetTarefaByIdAsync(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Tarefa tarefa)
+        {
+             await _repository.AddTarefaAsync(tarefa);
+             await _unitOfWork.CommitAsync();
+             return Ok(tarefa);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] Tarefa tarefa)
+        {
+            var tarefaEntity = await _repository.GetTarefaByIdAsync(id);
+            if (tarefaEntity == null)
+            {
+               return BadRequest();
+            }
+            _repository.UpdateTarefa(tarefa);
+            await _unitOfWork.CommitAsync();
+            return Ok(tarefa);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var tarefaEntity = await _repository.GetTarefaByIdAsync(id);
+            if (tarefaEntity == null)
+            {
+                return BadRequest();
+            }
+                
+            _repository.DeleteTarefa(tarefaEntity);
+            await _unitOfWork.CommitAsync();
+            return Ok();
+        }
+    }
+}
---
+++ Praticas/ListaTarefas/src/ListaTarefas.Api/Migrations/20250405044300_inicialMig.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ListaTarefas.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class inicialMig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Tarefas",
+                columns: table => new
+                {
+                    TarefaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Descricao = table.Column<string>(type: "varchar(250)", maxLength: 250, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    DataInicio = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    DataAlteracao = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tarefas", x => x.TarefaId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tarefas");
+        }
+    }
+}
